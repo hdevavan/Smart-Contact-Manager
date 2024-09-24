a49a2f0ccca9ccac11481c499f74cb604b93428a
hdevavan <hdevavan@gmail.com>
2024-09-24 17:35:48 +0530

    added bootstrap and common template using th:fragment

--- src/main/java/com/smart/controllers/HomeController.java
+++ src/main/java/com/smart/controllers/HomeController.java
@@ -4,33 +4,28 @@ import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
     private UserRepository userRepo;
 
     @RequestMapping("/home")
-    @ResponseBody
-    public User home() {
-        User user = new User();
-        user.setId(1);
-        user.setName("Harsh Devavanshi");
-        user.setEmail("harsh@example.com");
-        user.setPassword("password123");
-        user.setImage("profile.jpg");
-        user.setAbout("Full Stack Developer from New Delhi");
-        user.setRole("ROLE_USER");
-        user.setEnabled(true);
-        Contact c = new Contact();
-        user.getContacts().add(c);
-
-        return userRepo.save(user);
+    public String home(Model model) {
+        model.addAttribute("title","Home - Smart Contact Manager");
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title","About - Smart Contact Manager");
+        return "about";
     }
 }
